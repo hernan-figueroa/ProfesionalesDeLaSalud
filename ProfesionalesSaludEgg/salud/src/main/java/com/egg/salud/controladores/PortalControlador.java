654ccad15916d29032c7b80e6e92b98a1a88970f
Hernan Figueroa <gabriel_hernan_2107@hotmail.com>
2022-11-28 19:47:53 -0300

    update config

--- ProfesionalesSaludEgg/salud/src/main/java/com/egg/salud/controladores/PortalControlador.java
+++ ProfesionalesSaludEgg/salud/src/main/java/com/egg/salud/controladores/PortalControlador.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+
 public class PortalControlador {
     
 @Autowired
